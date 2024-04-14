8b5bc1dea6b37d9e3c14a89387d2fc5d96c8f9cd
imbolc <me@imbolc.name>
2024-04-14 15:54:59 +0600

    Cleanup

--- events/src/lib.rs
+++ events/src/lib.rs
@@ -1,6 +1,5 @@
 #![recursion_limit = "1024"]
 
-// use console_error_panic_hook::set_once as set_panic_hook;
 use wasm_bindgen::prelude::*;
 use web_sys::{window, Document, HtmlElement, HtmlInputElement, InputEvent};
 
